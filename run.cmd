8d3cda4093ddd712c24369953cb29bc1c4dbaab7
thomaswoehlke <thomas.woehlke@gmail.com>
2021-12-29 21:35:01 +0100

    work

---
+++ run.cmd
@@ -0,0 +1,9 @@
+
+set FLASK_APP=app.py
+set FLASK_ENV=development
+
+start /max cmd /t:8f /k "flask --help"
+
+rem start /max cmd /t:8f /k "celery --app app.celery worker --pool eventlet --loglevel INFO"
+
+start /max cmd /t:90 /k "flask run"
