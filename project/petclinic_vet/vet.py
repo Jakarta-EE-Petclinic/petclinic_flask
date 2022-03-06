9cb1569fb1f08dbf8fee0aa0ac4932e3dd760f99
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-06 16:33:40 +0100

    work

--- project/petclinic_vet/vet.py
+++ project/petclinic_vet/vet.py
@@ -6,7 +6,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 
 
-
 class VetForm(FlaskForm):
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
