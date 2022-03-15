7e1ac49ab78b1d8f0db0f40c0daea6ca35d1e829
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-15 19:41:39 +0100

    work

--- project/petclinic_views.py
+++ project/petclinic_views.py
@@ -181,15 +181,9 @@ app_web_urls = ApplicationUrls()
 
 
 class DomainModelOwnerUrls:
-    """Use Cases petclinic_owner
-    usecase owner_search as uc6000
-    usecase owner_list as uc6001
-    usecase owner_change as uc6002
-    usecase owner_new as uc6003
-    usecase owner_add_new_pet as uc6004
-    usecase owner_change_pet as uc6005
-    usecase owner_remove_pet as uc6006
-    usecase owner_give_pet_to_another_owner as uc6007
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/owner.uml
     """
     def __init__(self):
         app.logger.info(" DomainModelOwnerUrls [init]")
@@ -328,12 +322,10 @@ domain_model_owner_urls = DomainModelOwnerUrls()
 
 
 class DomainModelPetUrls:
-    """Use Cases petclinic_pet
-    usecase pet_search as uc5000
-    usecase pet_list as uc5001
-    usecase pet_new as uc5002
-    usecase pet_change as uc5003
-    usecase pet_remove as uc5004"""
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/pet.uml
+    """
     def __init__(self):
         app.logger.info(" DomainModelPetUrls [init]")
 
@@ -410,17 +402,17 @@ domain_model_pet_urls = DomainModelPetUrls()
 
 
 class DomainModelVisitUrls:
-    """Use Cases petclinic_visit
-    usecase pet_list_visits as uc7000
-    usecase pet_add_visit_to_pet_and_vet as uc7001
-    usecase pet_change_visit_to_pet_and_vet as uc7002
-    usecase pet_remove_visit as uc7003"""
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/visit.uml
+    """
     def __init__(self):
         app.logger.info(" DomainModelVisitUrls [init]")
 
     @staticmethod
     @app.route("/visit")
     def url_visit_index():
+        """"""
         page = request.args.get('page', 1, type=int)
         page_info = WebPageContent("petclinic_visit", "index")
         page_data = Visit.get_all(page)
@@ -433,6 +425,7 @@ class DomainModelVisitUrls:
     @staticmethod
     @app.route("/visit/new", methods=['GET', 'POST'])
     def url_visit_new():
+        """"""
         form = VisitForm()
         if request.method == 'POST' and form.validate_on_submit():
             o = Visit()
@@ -455,12 +448,10 @@ domain_model_visit_urls = DomainModelVisitUrls()
 
 
 class DomainModelPetTypeUrls:
-    """Use Cases petclinic_pettype
-    usecase pettype_search as uc4000
-    usecase pettype_list as uc4001
-    usecase pettype_new as uc4002
-    usecase pettype_change as uc4003
-    usecase pettype_remove as uc4004"""
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/pettype.uml
+    """
     def __init__(self):
         app.logger.info(" DomainModelPetTypeUrls [init]")
 
@@ -533,13 +524,10 @@ domain_model_pettype_urls = DomainModelPetTypeUrls()
 
 
 class DomainModelVetUrls:
-    """Use Cases petclinic_vet
-        usecase vet_search as uc3000
-        usecase vet_list as uc3001
-        usecase vet_new as uc3002
-        usecase vet_change as uc3003
-        usecase vet_choose_specialties as uc3004
-        usecase vet_remove as uc3005"""
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/vet.uml
+    """
     def __init__(self):
         app.logger.info(" DomainModelVetUrls [init]")
 
@@ -582,12 +570,10 @@ domain_model_vet_urls = DomainModelVetUrls()
 
 
 class DomainModelSpecialtyUrls:
-    """Use Cases petclinic_specialty
-    usecase specialty_search as uc2000
-    usecase specialty_list as uc2001
-    usecase specialty_new as uc2002
-    usecase specialty_change as uc2003
-    usecase specialty_remove as uc2004"""
+    """
+    .. uml:: petclinic_model/entities.uml
+    .. uml:: petclinic_model/specialty.uml
+    """
     def __init__(self):
         app.logger.info(" DomainModelSpecialtyUrls [init]")
 
