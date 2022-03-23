c5a21c7859747995e1213e266a3a6eba3c9f3635
thomaswoehlke <thomas.woehlke@gmail.com>
2022-03-23 12:27:59 +0100

    work

--- project/app_web/search.py
+++ project/app_web/search.py
@@ -2,6 +2,7 @@ from wtforms import SubmitField, SearchField
 
 from project.app_config.database import ModelForm
 
+
 class SearchForm(ModelForm):
     """
     .. uml:: entities.uml
@@ -9,3 +10,6 @@ class SearchForm(ModelForm):
     searchterm = SearchField()
 
     submit = SubmitField('Search')
+
+    def validate_on_submit(self):
+       return (self.searchterm.data != "")
